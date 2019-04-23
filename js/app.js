6c1023277cd6355ffa7a9e9c92b8bb43e0b654ad
Leonardo Muros do Nascimento <lmuros@gmail.com>
2019-04-22 23:30:19 -0300

    Sugestão de melhorias da mentoria

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 //Controle da inicialização do jogo
+'use strict'
 class Gerenciador{
 	constructor(inicio){
 		this.inicio = inicio;
@@ -12,30 +13,34 @@ class Gerenciador{
 class Enemy extends Gerenciador{
 	constructor(y, velocidade, inicio){
 		super(inicio);
-		this.x = -90 //posição horizontal do inimigo iniciando no canto esquerdo da tela
-		this.y = y; //posição vertical e fixa do inimigo
-		this.velocidade = velocidade; //velocidade do inimigo
+		this.width = 101; 					//largura horizontal entre o meio de um bloco e outro
+		this.interval = this.width / 2;		//metade da largura horizontal de um bloco
+		this.initial_x = -101; 				//posição horizontal do inimigo iniciando no canto esquerdo da tela
+		this.x = this.initial_x; 			//variável que terá a modificação constante de valor, permitindo o movimento
+		this.width_max = 550; 				//posição final do inimigo
+		this.fixed_y = y; 					//posição vertical e fixa do inimigo
+		this.velocidade = velocidade; 		//velocidade do inimigo, somado com a variável x, compõem o movimento
     	this.sprite = 'images/enemy-bug.png';
 	}
 	//Atualiza a posição do inimigo
 	update(dt){
-			for(let i = this.x + this.velocidade * dt; i <= 500; i++){
+			for(let i = this.x + this.velocidade * dt; i <= this.width_max; i++){
 			this.render(i);	//fazendo o inimigo se movimentar por meio do looping
 			this.colisao(i); //tratamento da colisão
-			if(i >= 490 && i <= 500){
-				i = -90 + this.velocidade * dt;
-				this.render(i);	//inimigo voltando à posição inicial para que a iteração acima possa reiniciar
+			if(i >= this.width_max - this.interval && i <= this.width_max){
+				i = this.initial_x + this.velocidade * dt;
+				this.render(i);	//inimigo voltando à posição inicial para que a iteração for possa reiniciar
 			}
 			break;
 		}
 	}
 	//Método que gerencia a colisão, trabalha em conjunto com o método update(dt)
 	colisao(x){
-		if(((x > -5 && x < 0) && (player.x === 3) && (player.y === this.y)) ||
-		  ((x > 59 && x < 63) && (player.x === 103) && (player.y === this.y)) ||
-		  ((x > 159 && x < 163) && (player.x === 203) && (player.y === this.y)) ||
-		  ((x > 259 && x < 263) && (player.x === 303) && (player.y === this.y)) ||
-		  ((x > 359 && x < 363) && (player.x === 403) && (player.y === this.y))){
+		if(((x > -this.interval && x < this.interval) && (player.x === 0) && (player.y === this.fixed_y)) ||
+		  ((x > this.interval && x < this.interval*3) && (player.x === player.width) && (player.y === this.fixed_y)) ||
+		  ((x > this.interval*3 && x < this.interval*5) && (player.x === player.width*2) && (player.y === this.fixed_y)) ||
+		  ((x > this.interval*5 && x < this.interval*7) && (player.x === player.width*3) && (player.y === this.fixed_y)) ||
+		  ((x > this.interval*7 && x < this.interval*9) && (player.x === player.width*4) && (player.y === this.fixed_y))){
 			player.y = player.update();
 		}
 	}
@@ -43,9 +48,9 @@ class Enemy extends Gerenciador{
 	render(movimento){
 		if(this.inicio === true){
 			if(movimento === undefined){
-				ctx.drawImage(Resources.get(this.sprite), this.x = this.x, this.y);
+				ctx.drawImage(Resources.get(this.sprite), this.x = this.x, this.fixed_y);
 			}else{
-				ctx.drawImage(Resources.get(this.sprite), this.x = movimento, this.y);
+				ctx.drawImage(Resources.get(this.sprite), this.x = movimento, this.fixed_y);
 			}
 		}
 	}
@@ -54,15 +59,23 @@ class Enemy extends Gerenciador{
 class Player extends Gerenciador{
 	constructor(inicio){
 		super(inicio);
-		this.x = 203;
-		this.y = 400;
+		this.initial_x = 202;
+		this.initial_y = 400;
+		this.x = this.initial_x;
+		this.y = this.initial_y;
+		this.width = 101;
+		this.heigth = 83
+		this.width_min = 0;
+		this.width_max = 404;
+		this.heigth_min = 50;
+		this.heigth_max = 400;
 		this.sprite = 'images/char-boy.png';
 		this.pontos = 0;
 		document.getElementById("conteudo").value = this.pontos;
 	}
 	//Atualiza com a posição inicial do jogador
 	update(){
-		return 400;
+		return this.initial_y;
 	}
 	//Desenhando o inimigo na tela após confirmação obtida da variável início
 	render(x, y) {
@@ -72,22 +85,25 @@ class Player extends Gerenciador{
 	}
 	//Configuração da direção a ser percorrida pelo jogador
 	handleInput(seta){
-		if((seta === 'left')&&(this.x >= 4)){
-			this.render(this.x = this.x - 100, this.y);
+		if((seta === 'left')&&(this.x > this.width_min)){
+			this.render(this.x = this.x - this.width, this.y); //Indo para a esquerda
 		}
-		if((seta === 'up')&&(this.y >= 50)){
-			this.render(this.x, this.y = this.y - 83);
-			if(this.y <= -15){
+		//---------------------------------------------------------------------
+		if((seta === 'up')&&(this.y >= this.heigth_min)){
+			this.render(this.x, this.y = this.y - this.heigth); //Indo pra cima
+			if(this.y <= this.heigth_min){
 				this.pontos += 1;
-				alert("Total de pontos: " + this.pontos);
-				this.render(this.x, this.y = this.update());
+				alert("Total de pontos: " + this.pontos); // Em uma determinada posição pra cima, a pontuação tem incremento
+				this.render(this.x, this.y = this.update()); //O jogador é realocado na posição de partida
 			}
 		}
-		if((seta === 'right')&&(this.x <= 303)){
-			this.render(this.x = this.x + 100, this.y);
+		//----------------------------------------------------------------------
+		if((seta === 'right')&&(this.x < this.width_max)){
+			this.render(this.x = this.x + this.width, this.initial_y); //Indo pra direita
 		}
-		if((seta === 'down')&&(this.y <= 350)){
-			this.render(this.x, this.y = this.y + 83);
+		//----------------------------------------------------------------------
+		if((seta === 'down')&&(this.y < this.heigth_max)){
+			this.render(this.x, this.y = this.y + this.heigth); //Indo pra baixo
 		}
 		document.getElementById("conteudo").value = this.pontos;
 	}
